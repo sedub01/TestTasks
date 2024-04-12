96d072f8196b2f8a2250d11be9056b097c98fd06
Simon Dubenkov <s.dubenkov@gk-adept.ru>
2024-04-12 14:19:39 +0300

    Add postreview changes

--- gaijin/client.cpp
+++ gaijin/client.cpp
@@ -12,30 +12,32 @@ using std::string;
 const string PORT_NUMBER = "1234";
 const string GET_KEY = "$get";
 const string SET_KEY = "$set";
+const int ITERATIONS = 10000;
+const string keys[] = {
+    "inspection",
+    "importance",
+    "percentage",
+    "fratricide",
+    "assignment",
+    "adjustment",
+    "blackboard",
+    "girlfriend",
+    "philosophy",
+    "literature"
+};
+const int SIZE = sizeof(keys) / sizeof(string);
 
-// Функция для выполнения операции $get или $set на сервере
-void performOperation(tcp::socket& socket, const string& operation) {
-    boost::asio::write(socket, boost::asio::buffer(operation + "\n"));
-    boost::asio::streambuf buffer;
-    boost::asio::read_until(socket, buffer, "\n");
-    string response = boost::asio::buffer_cast<const char*>(buffer.data());
-    response.pop_back(); // Удаляет '\n'
-    cout << "Server response: " << response << endl;
-}
+class Client {
+public:
+    Client(io_service& service, const string& address, const string& port)
+        : socket(service), resolver(service), endpoint_iterator(resolver.resolve({address, port})) {}
 
-int main() {
-    boost::asio::io_service service;
-    tcp::resolver resolver(service);
-    tcp::resolver::query query("localhost", PORT_NUMBER);
-    tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-    tcp::socket socket(service);
-
-    try {
-        connect(socket, endpoint_iterator);
+    void start() {
+        connect();
         srand(time(NULL));
-        for (int i = 0; i < 10000; ++i) {
+        for (int i = 0; i < ITERATIONS; ++i) {
             int operationType = rand() % 100;
-            string key = "key" + std::to_string(rand() % 10); // Случайный ключ от "key0" до "key9"
+            string key = keys[rand() % SIZE];
             string operation;
             // 99% вероятность для $get и 1% для $set
             if (operationType < 99) {
@@ -45,12 +47,43 @@ int main() {
                 operation = SET_KEY + " " + key + "=" + value;
             }
             cout << "Sending operation: " << operation << endl;
-            performOperation(socket, operation);
+            performOperation(operation);
         }
 
         cout << "The end!" << endl;
+    }
+
+    void shutdown() {
         socket.shutdown(tcp::socket::shutdown_both);
         socket.close();
+    }
+
+private:
+    tcp::socket socket;
+    tcp::resolver resolver;
+    tcp::resolver::iterator endpoint_iterator;
+
+    void connect() {
+        boost::asio::connect(socket, endpoint_iterator);
+    }
+
+    // Функция для выполнения операции $get или $set на сервере
+    void performOperation(const string& operation) {
+        boost::asio::write(socket, boost::asio::buffer(operation + "\n"));
+        boost::asio::streambuf buffer;
+        boost::asio::read_until(socket, buffer, "\n");
+        string response = boost::asio::buffer_cast<const char*>(buffer.data());
+        response.pop_back(); // Удаляет '\n'
+        cout << "Server response: " << response << endl;
+    }
+};
+
+int main() {
+    try {
+        boost::asio::io_service service;
+        Client client(service, "localhost", PORT_NUMBER);
+        client.start();
+        client.shutdown();
     } catch (std::exception& e) {
         cout << "Exception: " << e.what() << endl;
     }
--- gaijin/server.cpp
+++ gaijin/server.cpp
@@ -10,106 +10,117 @@ using ip::tcp;
 using std::cout;
 using std::endl;
 using std::string;
-using std::map;
+using std::unordered_map;
 using std::ifstream;
 using std::ofstream;
 using std::thread;
 using std::mutex;
 using std::lock_guard;
+using std::atomic;
 
 const int PORT_NUMBER = 1234;
 const string GET_KEY = "$get";
 const string SET_KEY = "$set";
 const char* FILENAME = "config.txt";
 
-// Глобальная переменная для хранения данных ключ/значение
-map<string, string> data;
-mutex dataMutex;
-int total_requests = 0;
-int requests_last_5_seconds = 0;
+class Server {
+public:
+    Server() : total_requests(0), requests_last_5_seconds(0) {}
 
-// Функция для чтения конфигурационного файла
-void readConfigFile() {
-    ifstream configFile(FILENAME);
-    string key, value;
-    while (configFile >> key >> value) {
-        data[key] = value;
-    }
-    configFile.close();
-}
+    void start() {
+        readConfigFile();
+        // Запуск потока для вывода статистики
+        thread statisticsThread(&Server::printStatistics, this);
 
-// Функция для обновления конфигурационного файла на диске
-void updateConfigFile() {
-    ofstream configFile(FILENAME);
-    for (const auto& pair : data) {
-        configFile << pair.first << " " << pair.second << endl;
-    }
-    configFile.close();
-}
+        io_service service;
+        tcp::endpoint endpoint(tcp::v4(), PORT_NUMBER);
+        tcp::acceptor acceptor(service, endpoint);
 
-// Функция для обработки клиентских запросов
-void handleClient(tcp::socket socket) {
-    try {
+        cout << "Server started" << endl;
+
         while (true) {
-            boost::asio::streambuf buffer;
-            boost::asio::read_until(socket, buffer, "\n");
-            string message = boost::asio::buffer_cast<const char*>(buffer.data());
-            message.pop_back(); // Удаляет '\n'
+            tcp::socket socket(service);
+            acceptor.accept(socket);
+            thread(&Server::handleClient, this, std::move(socket)).detach();
+        }
+    }
 
-            if (message.substr(0, 4) == GET_KEY) {
-                string key = message.substr(5);
-                string response;
-                lock_guard<mutex> guard(dataMutex);
-                response = data.find(key) != data.end() ? data[key] : "Key not found";
-                boost::asio::write(socket, boost::asio::buffer(response + "\n"));
-            } else if (message.substr(0, 4) == SET_KEY) {
-                string key_value = message.substr(5);
-                string key = key_value.substr(0, key_value.find("="));
-                string value = key_value.substr(key_value.find("=") + 1);
-                lock_guard<mutex> guard(dataMutex);
-                data[key] = value;
-                updateConfigFile();
-                boost::asio::write(socket, boost::asio::buffer("Value set\n"));
+private:
+    unordered_map<string, string> data;
+    mutex dataMutex;
+    atomic<int> total_requests;
+    atomic<int> requests_last_5_seconds;
+
+    // Функция для чтения конфигурационного файла
+    void readConfigFile() {
+        ifstream configFile(FILENAME);
+        string key, value;
+        while (configFile >> key >> value) {
+            data[key] = value;
+        }
+        configFile.close();
+    }
+
+    // Функция для обновления конфигурационного файла на диске
+    void updateConfigFile() {
+        ofstream configFile(FILENAME);
+        for (const auto& pair : data) {
+            configFile << pair.first << " " << pair.second << endl;
+        }
+        configFile.close();
+    }
+
+    // Функция для обработки клиентских запросов
+    void handleClient(tcp::socket socket) {
+        try {
+            while (true) {
+                boost::asio::streambuf buffer;
+                boost::asio::read_until(socket, buffer, "\n");
+                string message = boost::asio::buffer_cast<const char*>(buffer.data());
+                message.pop_back(); // Удаляет '\n'
+
+                if (message.substr(0, 4) == GET_KEY) {
+                    string key = message.substr(5);
+                    string response;
+                    lock_guard<mutex> guard(dataMutex);
+                    const auto it = data.find(key);
+                    response = it != data.end() ? it->second : "Key not found";
+                    boost::asio::write(socket, boost::asio::buffer(response + "\n"));
+                } else if (message.substr(0, 4) == SET_KEY) {
+                    string key_value = message.substr(5);
+                    string key = key_value.substr(0, key_value.find("="));
+                    string value = key_value.substr(key_value.find("=") + 1);
+                    lock_guard<mutex> guard(dataMutex);
+                    data[key] = value;
+                    updateConfigFile();
+                    boost::asio::write(socket, boost::asio::buffer("Value set\n"));
+                }
+
+                //Увеличиваем счетчики
+                total_requests++;
+                requests_last_5_seconds++;
             }
-
-            //Увеличиваем счетчики
-            total_requests++;
-            requests_last_5_seconds++;
-        }
-    } catch (boost::system::system_error& e) {
-        // Выводит сообщение об ошибке только если это не ошибка EOF
-        if (e.code() != boost::asio::error::eof) {
-            cout << "Exception in thread: " << e.what() << endl;
+        } catch (boost::system::system_error& e) {
+            // Выводит сообщение об ошибке только если это не ошибка EOF
+            if (e.code() != boost::asio::error::eof) {
+                cout << "Exception in thread: " << e.what() << endl;
+            }
         }
     }
-}
 
-// Функция для вывода статистики
-void printStatistics() {
-    while (true) {
-        cout << "Total requests: " << total_requests << ", last 5 seconds: " << requests_last_5_seconds << endl;
-        requests_last_5_seconds = 0; // Зануляет счетчик за последние 5 секунд
-        std::this_thread::sleep_for(std::chrono::seconds(5));
+    // Функция для вывода статистики
+    void printStatistics() {
+        while (true) {
+            cout << "Total requests: " << total_requests << ", last 5 seconds: " << requests_last_5_seconds << endl;
+            requests_last_5_seconds = 0; // Зануляет счетчик за последние 5 секунд
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+        }
     }
-}
+};
+
 
 int main() {
-    readConfigFile();
-
-    // Запуск потока для вывода статистики
-    thread statisticsThread(printStatistics);
-
-    io_service service;
-    tcp::endpoint endpoint(tcp::v4(), PORT_NUMBER);
-    tcp::acceptor acceptor(service, endpoint);
-
-    cout << "Server started" << endl;
-
-    while (true) {
-        tcp::socket socket(service);
-        acceptor.accept(socket);
-        thread(handleClient, std::move(socket)).detach();
-    }
-
+    Server server;
+    server.start();
     return 0;
 }
